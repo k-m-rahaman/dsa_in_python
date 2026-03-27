c9c1307c681544220c699bc8e2eba387863be562
k-m-rahaman <kmrofficial786@gmail.com>
2026-03-27 07:33:36 +0530

    Added Arrays module with basic operations in Python

---
+++ Arrays/README.md
@@ -0,0 +1,9 @@
+# Arrays
+
+This folder contains basic array operations in Python.
+
+## Topics
+- Traversal
+- Insertion
+- Deletion
+- Largest element
---
+++ Arrays/array_operations.py
@@ -0,0 +1,14 @@
+arr = [10, 20, 30, 40]
+
+# Traversal
+print("Traversal:")
+for i in arr:
+    print(i)
+
+# Insertion
+arr.append(50)
+print("After insertion:", arr)
+
+# Deletion
+arr.remove(20)
+print("After deletion:", arr)
